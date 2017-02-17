cc83e1759f3e4ae9186304aa883ee6dd1201a58a
Eric Jimenez <eric.jimenez.dev@gmail.com>
2017-02-16 20:48:10 -0500

    add static assets, image, and /logo route

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,22 @@
 import Link from 'next/link';
 import React from 'react';
 
-export default class Home extends React.Component {
-  render () {
-    return (<div>Hello World. <Link href='/users'><a>Users</a></Link></div>);
+export default() => {
+    return (
+      <div className="hello">Hello World. 
+        <Link href='/users'><a>Users</a></Link>
+        <style jsx>{`
+          .hello {
+            font: 15px Helvetica, Arial, sans-serif;
+            background: #eee;
+            padding: 100px;
+            text-align: center;
+            transition: 100ms ease-in background;
+          }
+          .hello:hover {
+            background: #ccc;
+          }
+        `}</style>
+      </div>
+      );
   }
-}
---
+++ pages/logo/index.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+import React from 'react';
+
+export default class Home extends React.Component {
+  render () {
+    return (
+    <div>
+      <img src="../../static/logo.svg" alt="react-logo"/>
+    </div>
+    );
+  }
+}
